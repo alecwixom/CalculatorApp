ca206bf89ffd53d4cfadf8b80807b40fcce33043
Alec Wixom <atwixom@gmail.com>
2023-06-16 16:20:41 -0600

    Removed eval() from the solveEquation(Line 21-30) and used mathjs instead to prevent issues

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+const math = require('mathjs')
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+
+  const buttonHandler = (e) => {
+    const buttonValue = e.target.textContent;
+    setInputValue((prevValue) => prevValue + buttonValue);
+  };
+
+  const clearInput = () => {
+    setInputValue("");
+  };
+
+  const backInput = () => {
+    setInputValue(inputValue.substring(0, inputValue.length -1))
+  }
+
+  const solveEquation = () => {
+    try {
+      const parsedExpression = math.parse(inputValue)
+      const evalutedResult = parsedExpression.evaluate()
+      
+      setInputValue(evalutedResult.toString());
+    } catch (error) {
+      setInputValue("Enter equation :(");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <div className="calcContainer">
+          <input
+            className="inputField"
+            type="text"
+            value={inputValue}
+            placeholder="Calculating. . ."
+          />
+          <div className="calcBtnsContainer">
+            <button onClick={buttonHandler}>1</button>
+            <button onClick={buttonHandler}>2</button>
+            <button onClick={buttonHandler}>3</button>
+            <button onClick={buttonHandler}>4</button>
+            <button onClick={buttonHandler}>5</button>
+            <button onClick={buttonHandler}>6</button>
+            <button onClick={buttonHandler}>7</button>
+            <button onClick={buttonHandler}>8</button>
+            <button onClick={buttonHandler}>9</button>
+            <button onClick={buttonHandler}>0</button>
+            <button onClick={buttonHandler}>+</button>
+            <button onClick={buttonHandler}>-</button>
+            <button onClick={buttonHandler}>*</button>
+            <button onClick={buttonHandler}>/</button>
+            <button onClick={buttonHandler}>(</button>
+            <button onClick={buttonHandler}>)</button>
+          </div>
+          <button onClick={clearInput} className="btnClear">Clear</button>
+          <button onClick={solveEquation} className="btnSolve">Solve</button>
+          <button onClick={backInput}>Back</button>
+        </div>
+      </section>
     </div>
   );
 }
